7252c401781f269319812a8d351784ee75424e66
William Alves <williamcostacardoso94@gmail.com>
2022-09-05 13:15:08 -0300

    TodoList TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem/index';
+import { AddArea } from './components/AddArea'
+
+const App = () => {
+
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'Comprar 1', done: false},
+    {id: 2, name: 'Comprar 2', done: true},
+  ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+
+  return (
+    <C.Container>
+        <C.Area>
+          <C.Header>Lista de Tarefas</C.Header>
+
+          <AddArea onEnter={ handleAddTask }/>
+
+          {list.map((item, index) => (
+            <ListItem key={ index } item={ item }/>
+          ))}            
+
+        </C.Area>
+    </C.Container>
+  );
+}
+
+export default App;
